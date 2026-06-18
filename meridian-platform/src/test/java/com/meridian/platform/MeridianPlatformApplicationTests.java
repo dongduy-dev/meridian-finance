4b6e63a0cbcbb7858d2431d8f5d9cab62808f27d
dongduy-dev <dongduy.dev@gmail.com>
2026-06-18 16:15:14 +0700

    chore: bootstrap backend runtime

---
+++ meridian-platform/src/test/java/com/meridian/platform/MeridianPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meridian.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeridianPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
